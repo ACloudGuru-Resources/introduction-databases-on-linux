8c1a9debe3717ec2902c779d41007631777eb1d7
Peter Ksiazek <peterktech@users.noreply.github.com>
2022-08-26 09:05:18 +1000

    Update sleep command

--- setup-sql-server.sh
+++ setup-sql-server.sh
@@ -5,7 +5,7 @@ if ! test -f "/home/cloud_user/database_passwd.txt"; then
     export MSSQL_SA_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32)
     echo $MSSQL_SA_PASSWORD > /home/cloud_user/database_passwd.txt
     /opt/mssql/bin/mssql-conf -n set-sa-password
-    sleep 60
+    sleep 120
     systemctl start mssql-server
     /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -Q 'CREATE DATABASE demo'
     curl -O -L https://github.com/linuxacademy/content-intro-to-databases-on-linux/raw/master/demo.sql
